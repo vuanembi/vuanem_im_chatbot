5493c7e26391591eb0dd93fc1f0ba9017908862b
HM <hieumdd@gmail.com>
2021-06-08 15:46:12 +0700

    Final Slack commit

--- main.py
+++ main.py
@@ -2,74 +2,83 @@ import os
 import requests
 
 from models import Report
+from pexecute.thread import ThreadLoom
 
 
-def create_sales_report(channel_id='C022WMH56KA', mode='daily'):
+def create_sales_report(channel_id="C022WMH56KA", mode="daily"):
     sales_order = (
-    "SalesOrder",
-    [
-        ("SalesOrder", "SUM"),
-        ("Transactions", "SUM"),
-        ("AOVCustomers", "AVG"),
-        ("AUSPMattress", "AVG"),
-        ("StoreTraffic", "SUM"),
-    ],
-)
+        "SalesOrder",
+        [
+            ("SalesOrder", "SUM"),
+            ("Transactions", "SUM"),
+            ("AUSPMattress", "AVG", "SalesOrderMattress", "QuantityMattress"),
+            ("StoreTraffic", "SUM"),
+        ],
+    )
     customers = (
-    "Customers",
-    [
-        ("Customers", "SUM"),
-        ("NewCustomers", "SUM"),
-    ],
-)
+        "Customers",
+        [
+            ("Customers", "SUM"),
+            ("NewCustomers", "SUM"),
+            ("AOVCustomers", "AVG", "SalesOrder", "Customers"),
+        ],
+    )
     report = Report.create("Sales", mode, channel_id)
     report.add_section(*sales_order)
     report.add_section(*customers)
     return report
 
-def create_profit_report(channel_id='C022WMH56KA', mode='daily'):
+
+def create_profit_report(channel_id="C022WMH56KA", mode="daily"):
     profit = (
         "Profit",
         [
             ("Sales", "SUM"),
             ("COGS", "SUM"),
             ("GrossProfit", "SUM"),
-            ("GrossMargin", "AVG"),
+            ("GrossMargin", "AVG", "GrossProfit", "Sales"),
         ],
     )
     report = Report.create("Profit", mode, channel_id)
     report.add_section(*profit)
     return report
-    
-def create_marketing_report(channel_id='C022WMH56KA', mode='daily'):
+
+
+def create_marketing_report(channel_id="C022WMH56KA", mode="daily"):
     spend = (
         "Spend",
-        [
-            ("FacebookSpend", "SUM"),
-            ("GoogleSpend", "SUM"),
-        ],
+        [("FacebookSpend", "SUM"), ("GoogleSpend", "SUM"), ("FunnelSpend", "SUM")],
     )
-    funnel = (
-        "Spend",
+    leads = (
+        "Leads",
         [
             ("TotalLeads", "SUM"),
+            ("UniqueLeads", "SUM"),
             ("PhonesCollected", "SUM"),
+            ("CostPerLead", "AVG", "FunnelSpend", "TotalLeads"),
+        ],
+    )
+    conversions = (
+        "Conversions",
+        [
             ("AcquiredCustomers", "SUM"),
-            ("FunnelCR", "AVG"),
+            ("FunnelCR", "AVG", "AcquiredCustomers", "PhonesCollected"),
             ("FunnelRevenue", "SUM"),
+            ("ROAS", "AVG", "FunnelRevenue", "FunnelSpend"),
         ],
     )
     report = Report.create("Marketing", mode, channel_id)
     report.add_section(*spend)
-    report.add_section(*funnel)
+    report.add_section(*leads)
+    report.add_section(*conversions)
     return report
 
 
 def report_factory(mode):
     reports = []
     if mode == "daily":
-        reports.append(create_sales_report(mode=mode))
-        reports.append(create_profit_report(mode=mode))
+        # reports.append(create_sales_report(mode=mode))
+        # reports.append(create_profit_report(mode=mode))
         reports.append(create_marketing_report(mode=mode))
     elif mode == "realtime":
         reports.append(create_sales_report(mode=mode))
@@ -97,8 +106,12 @@ def main(request):
     if request_json:
         mode = request_json["mode"]
         reports = report_factory(mode)
+        loom = ThreadLoom(max_runner_cap=10)
         for report in reports:
             report.push()
+        #     loom.add_function(report.push)
+        # output =  loom.execute()
+        # return 
         return {"reports_pushed": len(reports)}
     else:
         raise RuntimeError("400 Bad Request")
--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import os
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from abc import abstractmethod, ABCMeta
 
 import jinja2
@@ -30,20 +30,20 @@ class Report(metaclass=ABCMeta):
         raise NotImplementedError
 
     def fetch_data(self):
-        headers = {
-            "Accept-Version": "v1"
-        }
-        params = {
-            "client_id": os.getenv('UNSPLASH_CLIENT_ID'),
-            "query": "burger",
-            "orientation": "squarish",
-            "content_filter": "low",
-            "count": len(self.sections)
-        }
-        with requests.get("https://api.unsplash.com/photos/random", headers=headers, params=params) as r:
-            res = r.json()
-        urls = [i['urls']['small'] for i in res]
-        rows = [section.build(urls[i]) for i, section in enumerate(self.sections)]
+        # headers = {"Accept-Version": "v1"}
+        # params = {
+        #     "client_id": os.getenv("UNSPLASH_CLIENT_ID"),
+        #     "query": "burger",
+        #     "orientation": "squarish",
+        #     "content_filter": "low",
+        #     "count": len(self.sections),
+        # }
+        # with requests.get(
+        #     "https://api.unsplash.com/photos/random", headers=headers, params=params
+        # ) as r:
+        #     res = r.json()
+        # urls = [i["urls"]["small"] for i in res]
+        rows = [section.build() for section in self.sections]
         return rows
 
     def build(self):
@@ -59,6 +59,7 @@ class Report(metaclass=ABCMeta):
             blocks.append({"type": "divider"})
             blocks.append(row)
         payload["blocks"] = blocks
+        payload
         return payload
 
     def compose_header(self):
@@ -70,9 +71,22 @@ class Report(metaclass=ABCMeta):
     def _compose_title(self):
         raise NotImplementedError
 
-    @abstractmethod
     def _compose_prelude(self):
-        raise NotImplementedError
+        query = '''
+        SELECT LastUpdated FROM Analytics._agg_Slack_Report
+        '''
+        rows = self.bq_client.query(query).result()
+        row = [row.values() for row in rows][0][0]
+        tz = timezone(timedelta(hours=7))
+        last_updated = row.astimezone(tz).strftime('%Y-%m-%dT%H:%M:%S%z')
+        return {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": f"Last Updated: {last_updated}",
+            },
+        }
+
 
     def push(self):
         token = os.getenv("TOKEN")
@@ -81,10 +95,14 @@ class Report(metaclass=ABCMeta):
             "Content-type": "application/json",
             "Authorization": f"Bearer {token}",
         }
+        payload = self.build()
+        payload
         with requests.post(
-            "https://slack.com/api/chat.postMessage", headers=headers, json=self.build()
+            "https://slack.com/api/chat.postMessage", headers=headers, json=payload
         ) as r:
-            r.raise_for_status()
+            res = r.json()
+        return res.get('ok')
+
 
 class ReportDaily(Report):
     def __init__(self, name, channel_id):
@@ -99,20 +117,10 @@ class ReportDaily(Report):
             "type": "header",
             "text": {
                 "type": "plain_text",
-                "text": f"Daily {self.report_name} Report {now:%Y-%m-%d}",
+                "text": f"{self.report_name} :: Daily :: {now:%Y-%m-%d}",
                 "emoji": True,
             },
         }
-
-    def _compose_prelude(self):
-        return {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": f"Báo cáo đầu ngày nên BI BOT ko dám ba hoa",
-            },
-        }
-
 
 class ReportRealtime(Report):
     def __init__(self, name, channel_id):
@@ -127,52 +135,31 @@ class ReportRealtime(Report):
             "type": "header",
             "text": {
                 "type": "plain_text",
-                "text": f"Realtime {self.name} Report {now:%Y-%m-%d}",
+                "text": f"{self.report_name} :: Realtime :: {now:%Y-%m-%d}",
                 "emoji": True,
             },
         }
-
-    def _compose_prelude(self):
-        weather_mess, temp = self._compose_temp()
-        return {
-            "type": "section",
-            "text": {
-                "type": "mrkdwn",
-                "text": f"Nhiệt độ Hà Nội hiện là {temp}°C. {weather_mess}\n*Đưa tay đây nào, nhận báo cáo bạn nhớ :D*",
-            },
-        }
-
-    def _compose_temp(self):
-        with requests.get(
-            "https://api.openweathermap.org/data/2.5/weather",
-            params={
-                "APPID": os.getenv("WEATHER_API_KEY"),
-                "id": "1581129",
-                "units": "metric",
-            },
-        ) as r:
-            res = r.json()
-        weather_cond = res["weather"][0]["id"]
-        if weather_cond <= 232:
-            weather_mess = "Sắp mưa to bão bùng kinh khủng :zap:. BI BOT đề xuất về sớm để tránh ngập lụt tát nước."
-        elif weather_cond >= 300 and weather_cond <= 531:
-            weather_mess = "Sắp mưa :umbrella:. BI BOT đề xuất bạn nào ở nhà quần áo đang phơi thì về sớm cất quần áo."
-        elif weather_cond == 800:
-            weather_mess = "Trời trong xanh :sunny:. BI BOT đề xuất nghỉ sớm để ra ngoài tận hưởng thời tiết."
-        elif weather_cond > 800:
-            weather_mess = (
-                "Trời trong xanh :cloud:. BI BOT đề xuất lên sân thượng ngắm thời tiết."
-            )
-
-        temp = res["main"]["temp"]
-        return weather_mess, temp
-
 
 class Section:
     def __init__(self, name, metrics, mode, bq_client):
         self.section_name = name
         self.bq_client = bq_client
-        self.metrics = [Metric.factory(metric[0], mode, metric[1]) for metric in metrics]
+        self.metrics = [
+            Metric.factory(
+                metric[0],
+                mode,
+                metric[1],
+                self.safe_index(metric, 2),
+                self.safe_index(metric, 3),
+            )
+            for metric in metrics
+        ]
+
+    def safe_index(self, metric, i):
+        try:
+            return metric[i]
+        except IndexError:
+            return None
 
     def fetch_data(self):
         loader = jinja2.FileSystemLoader(searchpath="./queries")
@@ -183,10 +170,10 @@ class Section:
         row = [row for row in rows][0]
         return row
 
-    def build(self, image):
+    def build(self):
         row = self.fetch_data()
         self.hydrate(row)
-        return self.compose(image)
+        return self.compose()
 
     def hydrate(self, row):
         for k, v in row.items():
@@ -195,7 +182,7 @@ class Section:
                     if metric.metric_name == k:
                         metric = metric.update({"metric_name": k, **v})
 
-    def compose(self, image):
+    def compose(self):
         head = self._compose_head()
         body = self._compose_body()
         return {
@@ -204,33 +191,30 @@ class Section:
                 "type": "mrkdwn",
                 "text": head,
             },
-            "fields": body,
-            "accessory": {
-                "type": "image",
-                "image_url": image,
-                "alt_text": "null",
-            },
+            "fields": body
         }
 
     def _compose_head(self):
-        return f"*{self.section_name}* :moneybag: :star: :moneybag:"
+        return f"*{self.section_name}*"
 
     def _compose_body(self):
         return [metric.build() for metric in self.metrics]
 
 
 class Metric(metaclass=ABCMeta):
-    def __init__(self, name, agg):
+    def __init__(self, name, agg, numerator, denominator):
         self.metric_name = name
         self.agg = agg
         self.title = f"*{self.metric_name}*"
+        self.numerator = numerator
+        self.denominator = denominator
 
     @staticmethod
-    def factory(name, mode, agg):
+    def factory(name, mode, agg, numerator, denominator):
         if mode == "realtime":
-            return MetricRealtime(name, agg)
+            return MetricRealtime(name, agg, numerator, denominator)
         elif mode == "daily":
-            return MetricDaily(name, agg)
+            return MetricDaily(name, agg, numerator, denominator)
         else:
             raise NotImplementedError("Metric mode not found")
 
@@ -257,22 +241,24 @@ class Metric(metaclass=ABCMeta):
 
     def _format_value(self, value):
         if value is None:
-            return f"Missing Value"
+            return f"null"
         elif value >= 1e9:
             return f"{value/1e9:.2f} B"
         elif value >= 1e6:
             return f"{value/1e6:.2f} M"
         elif value >= 1e3:
             return f"{value/1e3:.2f} K"
-        elif value <= 1:
+        elif value <= 1 and value >= -1:
             return f"{value*1e2:.2f} %"
+        elif isinstance(value, int):
+            return f"{value}"
         else:
-            return value
+            return f"{value:.2f}"
 
 
 class MetricRealtime(Metric):
-    def __init__(self, name, agg):
-        super().__init__(name, agg)
+    def __init__(self, name, agg, numerator, denominator):
+        super().__init__(name, agg, numerator, denominator)
 
     def _compose_text(self):
         self.format_value()
@@ -280,15 +266,17 @@ class MetricRealtime(Metric):
 
 
 class MetricDaily(Metric):
-    def __init__(self, name, agg):
-        super().__init__(name, agg)
+    def __init__(self, name, agg, numerator, denominator):
+        super().__init__(name, agg, numerator, denominator)
+        self.numerator = numerator
+        self.denominator = denominator
 
     def _compose_text(self):
         self.compare, self.emoji = self._compare()
         self.format_value()
-        dod = f"> :small_blue_diamond: Y-day: {self.d1}"
-        compare = f"> :small_blue_diamond: {self.emoji} {self.compare:.2f}%"
-        mtd = f"> :small_blue_diamond: MTD : {self.mtd}"
+        dod = f"> - Y-day: {self.d1}"
+        compare = f"> - {self.emoji} {self.compare:.2f}%"
+        mtd = f"> - MTD : {self.mtd}"
         text = "\n".join([dod, compare, mtd])
         return text
 
@@ -301,5 +289,5 @@ class MetricDaily(Metric):
                 emoji = "Giảm :small_red_triangle_down:"
         else:
             compare = 0
-            emoji = 'Missing Value'
+            emoji = "null"
         return compare, emoji
